fc893cb239fb3f1908e238ab240208be68cc0026
User <niedbalskiadrian@gmail.com>
2026-05-13 18:28:32 +0200

    Created Player and position for player

---
+++ src/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    private String name = "P";
+    private int lives = 3;
+    private Position position;
+    private int treasuresCollected = 0;
+    private boolean inBase = false;
+
+    public Player(int x, int y) {
+        this.position = new Position(x, y);
+    }
+}
